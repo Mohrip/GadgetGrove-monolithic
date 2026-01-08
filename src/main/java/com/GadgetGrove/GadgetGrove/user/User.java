6c1042c6bf73c2174dda39f0826fa83e104934c3
mohrip <m25srt@gmail.com>
2026-01-08 12:27:21 +0300

    Commit to apply JPA - GadgetGrove project

--- src/main/java/com/GadgetGrove/GadgetGrove/user/User.java
+++ src/main/java/com/GadgetGrove/GadgetGrove/user/User.java
@@ -1,10 +1,21 @@
 package com.GadgetGrove.GadgetGrove.user;
 
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Table(name = "users_table")
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
