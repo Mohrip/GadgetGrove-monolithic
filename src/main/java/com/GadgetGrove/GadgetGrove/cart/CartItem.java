3e36f23c7e2015d386236c23c07fed757bc17aa9
mohrip <m25srt@gmail.com>
2026-01-26 13:02:31 +0300

    Product service

--- src/main/java/com/GadgetGrove/GadgetGrove/cart/CartItem.java
+++ src/main/java/com/GadgetGrove/GadgetGrove/cart/CartItem.java
@@ -1,4 +1,38 @@
 package com.GadgetGrove.GadgetGrove.cart;
 
+
+import com.GadgetGrove.GadgetGrove.product.Product;
+import com.GadgetGrove.GadgetGrove.user.User;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
 public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+    private Integer quantity;
+    private BigDecimal price;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+
 }
