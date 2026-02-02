c00eae980c04f7bcc4bb6d5c58ce50840eaa03e4
mohrip <m25srt@gmail.com>
2026-02-02 16:03:42 +0300

    feat(Add Order): place Order

--- src/main/java/com/GadgetGrove/GadgetGrove/order/OrderItem.java
+++ src/main/java/com/GadgetGrove/GadgetGrove/order/OrderItem.java
@@ -1,4 +1,33 @@
 package com.GadgetGrove.GadgetGrove.order;
 
+import com.GadgetGrove.GadgetGrove.product.Product;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity(name = "order_items")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    private Integer quantity;
+
+    private BigDecimal price;
 }
