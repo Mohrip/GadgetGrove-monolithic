7d864f00b7145523c5d9f6504cf243e02ede13cd
mohrip <m25srt@gmail.com>
2026-02-02 16:07:52 +0300

    feat(Add Order): place Order

---
+++ src/main/java/com/GadgetGrove/GadgetGrove/order/OrderController.java
@@ -0,0 +1,23 @@
+package com.GadgetGrove.GadgetGrove.order;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<OrderResponse> placeOrder(
+            @RequestHeader("User-ID") UUID userId,
+            @RequestBody OrderRequest request) {
+        return new ResponseEntity<>(orderService.placeOrder(userId, request), HttpStatus.CREATED);
+    }
+}
